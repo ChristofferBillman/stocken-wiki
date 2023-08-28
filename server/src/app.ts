4504e94cab12f292c98570c0b33d1143c2fcf3fe
ChristofferBillman <christoffer.billman@gmail.com>
2023-08-28 10:10:06 +0200

    configured ssh

---
+++ server/src/app.ts
@@ -0,0 +1,50 @@
+import GetDatabaseConnection from './Database'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+import express, { Request, Response, Application } from 'express'
+import Token from './Token'
+import UserAPI from './api/UserAPI'
+import NoAuthAPI from './api/NoAuthAPI'
+import PageAPI from './api/PageAPI'
+import PageHistoryAPI from './api/PageHistoryAPI'
+import SearchAPI from './api/SearchAPI'
+
+GetDatabaseConnection()
+
+const app: Application = express()
+const port = process.env.PORT || 3000
+const BASEURL = '/api'
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: [
+        'http://wiki.christofferbillman.se',
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000"
+    ],
+    credentials: true
+}))
+
+app.listen(port, () => console.log(`Wiki server is running on port ${port}`))
+
+app.get(BASEURL + '/test', (req: Request, res: Response) => res.send('Hello: ' + req.query.name))
+
+NoAuthAPI(app, BASEURL)
+
+// cookieParser is run before Token.VerifyMiddleware
+// because it depends on the cookies being parsed.
+app.use(cookieParser())
+// This middleware will be invoked on all following routes.
+// All following routes will require a valid token.
+app.use(Token.VerifyAndAddIdToReq)
+
+UserAPI(app, BASEURL + '/user')
+PageAPI(app, BASEURL + '/page')
+PageHistoryAPI(app, BASEURL + '/page/history')
+SearchAPI(app, BASEURL)
+
+export default app;
+
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,48 +1,21 @@
-import GetDatabaseConnection from './Database'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+'use strict';
 
-import express, { Request, Response, Application } from 'express'
-import Token from './Token'
-import UserAPI from './api/UserAPI'
-import NoAuthAPI from './api/NoAuthAPI'
-import PageAPI from './api/PageAPI'
-import SearchAPI from './api/SearchAPI'
-import PageHistoryAPI from './api/PageHistoryAPI'
+import app from'./app'
 
-GetDatabaseConnection()
+import greenlock from 'greenlock-express'
 
-const app: Application = express()
-const port = process.env.PORT || 3000
-const BASEURL = '/api'
+    greenlock.init({
+        packageRoot: __dirname + '/..',
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-    origin: [
-        'http://wiki.stocken.okdev.se',
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000" 
-    ],
-    credentials: true
-}))
+        // contact for security and critical bug notices
+        maintainerEmail: "christoffer.billman@gmail.com",
 
-app.listen(port, () => console.log(`Wiki server is running on port ${port}`))
+        // where to look for configuration
+        configDir: './greenlock.d',
 
-app.get(BASEURL + '/test', (req: Request, res: Response) => res.send('Hello: ' + req.query.name))
-
-NoAuthAPI(app, BASEURL)
-
-// cookieParser is run before Token.VerifyMiddleware
-// because it depends on the cookies being parsed.
-app.use(cookieParser())
-// This middleware will be invoked on all following routes.
-// All following routes will require a valid token.
-app.use(Token.VerifyAndAddIdToReq)
-
-UserAPI(app, BASEURL + '/user')
-PageAPI(app, BASEURL + '/page')
-PageHistoryAPI(app, BASEURL + '/page/history')
-SearchAPI(app, BASEURL)
-
+        // whether or not to run at cloudscale
+        cluster: false
+    })
+    // Serves on 80 and 443
+    // Get's SSL certificates magically!
+    .serve(app);
