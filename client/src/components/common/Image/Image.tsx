17f678683c532b6dc7ef19b39bcdef2899c52e68
ChristofferBillman <christoffer.billman@gmail.com>
2023-09-07 22:40:42 +0200

    feat: images can be clicked to make them larger.

--- client/src/components/common/Image/Image.tsx
+++ client/src/components/common/Image/Image.tsx
@@ -5,6 +5,7 @@ import style from './Image.module.css'
 export default function Image({ src, alt, children }: any) {
 
 	const [loading, setLoading] = useState(true)
+	const [galleryVisible, setGalleryVisible] = useState(false)
 
 	return (
 		<>
@@ -14,9 +15,20 @@ export default function Image({ src, alt, children }: any) {
 				alt={alt}
 				style={{display: loading ? 'none' : 'block', width: '100%'}}
 				onLoad={() => setLoading(false)}
+				onClick={() => setGalleryVisible(true)}
 			>
 				{children}
 			</img>
+			<div className={style.gallery} style={{display: galleryVisible ? 'block' : 'none'}} onClick={() => setGalleryVisible(false)}>
+				<img
+					className={style.fullscreenImg}
+					style={{display: loading ? 'none' : 'block'}}
+					src={src}
+					alt={alt}
+				>
+					{children}
+				</img>
+			</div>
 		</>
 	)
 }
